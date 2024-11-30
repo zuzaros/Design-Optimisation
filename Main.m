d3192cdfbe95361162afce2ca1cd1aa3aedea62c
zuzaros <zr305@bath.ac.uk>
2024-11-30 16:27:44 +0000

    linear twist dist with GA

--- Main.m
+++ Main.m
@@ -2,13 +2,30 @@ clc
 clear
 close all
 
-
+%% SET UP
 %%% Open InitialiseScript and change the filepaths to match your system
 InitialiseScript %runs initialistion script for file paths and names
 
-f = @(X) objFunc(X,Params);
+rng default %for reproducibility
 
-X = fminbnd(f,-10,10); %efficient one-parameter, gradient-free optimiser
+%set a vector of lower and upper bounds for the optimisation
+lb = [-10,-10]; %lower bounds
+ub = [10,10]; %upper bounds
+
+% constraints for GA
+A = [];
+b = [];
+Aeq = [];
+beq = [];
+nonlcon = [];
+
+%set the number of variables and the objective function
+nvars = 2; %number of variables
+fun = @(X)objFunc(X,Params); %objective function
+
+%run the genetic algorithm
+options = optimoptions('ga','PlotFcn',@gaplotbestf,'MaxGenerations',100,'PopulationSize',100,'FunctionTolerance',1e-6);
+[x,fval,exitflag,output,population,scores] = ga(fun,nvars,A,b,Aeq,beq,lb,ub,nonlcon,options);
 
 databaseConnect(db_path,projectFilePath,'Disconnect')
 
@@ -23,11 +40,14 @@ function F = objFunc(X,Params)
     r = linspace(0,1,25); % radial position of blade in 25 steps
     lambda_r = lambda*r; % local tip speed ratio
 
-    newTwist = ones(25,1)*X(1); %creates a constant twist distribution of X(1) degrees
+    twist_root = X(1); % Twist at the root
+    twist_tip = X(2); % Twist at the tip
+    newTwist = linspace(twist_root, twist_tip, 25); % Linearly varying twist distribution
+
     newChord = optimalChord(numBlades,r,lambda_r);
 
       
-    disp(strcat('twist = ',num2str(X(1)),' degrees'))
+    disp(strcat('twist = ',num2str(X(1)),' at root and  ',num2str(X(2)),' degrees at tip'))
 
     Foils = {'naca2424'}; %specifies aerofoils to be used (must match name in Ashes aerofoil database)
     foilsDist = [ones(1,25)]; %specifies aerofoil distribution based on Foils index
